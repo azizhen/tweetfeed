3bf6b541455a9cfb6008b325c929246d7b39040b
azizhen <helicopter>
2016-05-20 17:30:29 +0200

    Added argument checking when running via jar command

--- src/main/java/co/za/hendricks/App.java
+++ src/main/java/co/za/hendricks/App.java
@@ -8,16 +8,16 @@ import co.za.hendricks.filemanagement.TwitterFileReader;
 import java.io.IOException;
 import java.util.List;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class App
 {
     public static void main( String[] args ) throws IOException {
 
-//        String userfilePath = "/users/aziz/Downloads/user.txt";
-//        String tweetfilePath = "/users/aziz/Downloads/tweet.txt";
+        checkArgument(args.length == 2, "Two arguments must be provided eg. \\path\\user.txt \\path\\tweet.txt");
 
         String userfilePath = args[0] ;
         String tweetfilePath = args[1];
-
 
         TwitterFileReader twitterFileReader = new TwitterFileReader(userfilePath, tweetfilePath);
         List<TwitterUser> twitterUsers = twitterFileReader.getTwitterUsers();
